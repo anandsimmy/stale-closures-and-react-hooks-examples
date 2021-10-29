9862831b2a81991e1c1951256d416af9067b5c3f
Anand Simmy <anandsimmy7@gmail.com>
2021-10-29 14:43:09 +0530

    Update solution-3.js

--- solution-3.js
+++ solution-3.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const titleRef = useRef();
   const countRef = useRef(count);
   countRef.current = count;
   useEffect(() => {
-    const countHeader = document.getElementById('count');
+    const countHeader = titleRef.current;
     const clickHandler = () => {
-      if (countRef.current > 0) {
+      if (countRef.current > 0 > 0) {
         setCount(100);
       } else {
         setCount(-100);
       }
     };
-    countHeader.addEventListener('click', clickHandler);
-    return () => countHeader.removeEventListener('click', clickHandler);
+    countHeader.addEventListener("click", clickHandler);
+    return () => countHeader.removeEventListener("click", clickHandler);
   }, []);
 
   return (
     <div>
-      <h1 id="count">Count: {count}</h1>
+      <h1 ref={titleRef}>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
     </div>
